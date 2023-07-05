7602657b2ce7993ae3a37cbb64e7440b87969ae5
abhishek-T99 <timilsina.abhishek55@gmail.com>
2023-07-05 10:27:42 +0545

    feat: Project Directory configuration and Add server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+const server = http.createServer(app);
+
+server.listen(port);
